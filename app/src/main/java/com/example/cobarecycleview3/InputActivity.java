337211b9c324ed4c0d1cd607ac1ca3de82ecf7fd
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-02-18 23:08:33 +0700

    progress add view layout

--- app/src/main/java/com/example/cobarecycleview3/InputActivity.java
+++ app/src/main/java/com/example/cobarecycleview3/InputActivity.java
@@ -1,14 +1,51 @@
 package com.example.cobarecycleview3;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cobarecycleview3.model.Hotel;
+
 public class InputActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_GET = 1;
+    EditText etJudul;
+    EditText etDeskripsi;
+    EditText etDetail;
+    EditText etLokasi;
+    ImageView ivFoto;
+    Uri uriFoto;
+    Hotel hotel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        etJudul = findViewById(R.id.editTextNama);
+        etDeskripsi = findViewById(R.id.editTextDeskripsi);
+        etDetail = findViewById(R.id.editTextDetail);
+        etLokasi = findViewById(R.id.editTextLokasi);
+        ivFoto = findViewById(R.id.imageViewFoto);
+
+        ivFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickPhoto();
+            }
+        });
+
+        findViewById(R.id.buttonSimpan).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        doSave();
+                    }
+                }
+        );
     }
 }
